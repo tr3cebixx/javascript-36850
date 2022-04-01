a0b3aae9c643ec1dbc1774ba8d45ff7c263c152e
GianGerardi <giangerardi99@gmail.com>
2022-04-01 18:53:47 -0300

    correccion del profe, sigue sin ejecutarse prueba.method, agrego prueba.arrays que tampoco es funcional

--- RPJavaScript/js/prueba.method.js
+++ RPJavaScript/js/prueba.method.js
@@ -5,7 +5,7 @@ class UserBase{
         this.clase = clase;
     }
     bienvenido(user){
-        let user = prompt("Bienvenido a RPJavaScript, defensor del Código. Elige tu nombre (con cuidado, todos lo recordarán!)");
+        this.user = prompt("Bienvenido a RPJavaScript, defensor del Código. Elige tu nombre (con cuidado, todos lo recordarán!)");
         
         if (this.user == "") {
             user = prompt("Estoy seguro de que tienes algún nombre, no seas tímido");
@@ -15,7 +15,7 @@ class UserBase{
         }
     }
     elegirRaza(raza){
-        let raza = prompt("Muy bien, " + this.user + " ahora cuentame, de que raza provienes?\nHumano\nOrco\nElfo\nEnano\nGoblin\nTrol\nDemonio");
+        this.raza = prompt("Muy bien, " + this.user + " ahora cuentame, de que raza provienes?\nHumano\nOrco\nElfo\nEnano\nGoblin\nTrol\nDemonio");
 
         if (this.raza !== "humano", "orco", "elfo", "enano", "goblin", "trol", "demonio") {
             raza = prompt("Esa no es una raza de este mundo! Elige una raza de verdad:\n Humano\n Orco\n Elfo\n Enano\n Goblin\n Trol\n Demonio");
@@ -25,7 +25,7 @@ class UserBase{
         }
     }
     elegirClase(clase){
-        let clase = prompt("En RPJS hay guerreros de distintas clases, que te enseñaran todo lo que necesitas para ser un gran defensor, así que deberas elegir una:\n Caballero\n Bersérker\n Paladín\n Cazador\n Asesino\n Mago\n Brujo\n Sacerdote\n Espadachín\n Caballero Oscuro\n Monje");
+        this.clase = prompt("En RPJS hay guerreros de distintas clases, que te enseñaran todo lo que necesitas para ser un gran defensor, así que deberas elegir una:\n Caballero\n Bersérker\n Paladín\n Cazador\n Asesino\n Mago\n Brujo\n Sacerdote\n Espadachín\n Caballero Oscuro\n Monje");
         
         if (this.clase !== "caballero", "bersérker", "paladín", "cazador", "asesino", "Mago", "Brujo", "sacerdote", "espadachín", "caballero oscuro", "monje") {
             clase = prompt("'" + this.clase + "'?? Aquí no tenemos instructores de eso... puedes elegir entre:\n Caballero\n Bersérker\n Paladín\n Cazador\n Asesino\n Mago\n Brujo\n Sacerdote\n Espadachín\n Caballero Oscuro\n Monje")
@@ -37,6 +37,10 @@ class UserBase{
 }
 
 const profile1 = new UserBase (user, raza, clase);
+
+profile1.bienvenido(user);
+profile1.elegirRaza(raza);
+profile1.elegirClase(clase);
 
 for (const data in profile1) {
     console.log(profile1[data]);
